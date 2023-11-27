7771defd6b8c5ee121ad7890ab48175f564f0dcd
exautinus <exautinus@gmail.com>
2023-11-27 19:15:15 -0300

    QR generator complete

--- src/app/pages/main/profile/profile.page.ts
+++ src/app/pages/main/profile/profile.page.ts
@@ -1,11 +1,8 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { FirebaseService } from 'src/app/services/firebase.service';
 import { UtilsService } from 'src/app/services/utils.service';
-import {BarcodeScanner} from '@awesome-cordova-plugins/barcode-scanner/ngx'
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-
-
-
+import * as QRCode from 'qrcode';
 
 @Component({
   selector: 'app-profile',
@@ -13,60 +10,80 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
-
+  // Formulario para la página del perfil del profesor
   form = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
+    materia: new FormControl('', Validators.required), // Campo de la materia con validación requerida
+  });
 
-  })
-
+  // Servicios inyectados
   firebaseSvc = inject(FirebaseService);
   utilsSvc = inject(UtilsService);
-  
 
-  ngOnInit() {
+  // Datos del código QR generado
+  qrCodeData: string = '';
+
+  // Estado para controlar el proceso de generación del código QR
+  isGeneratingQR: boolean = false;
+
+  ngOnInit() {}
+
+  // Función para generar el código QR
+  generateQRCode() {
+    // Verificar si se ha seleccionado una materia antes de generar el QR
+    if (this.form.controls.materia.valid) {
+      this.isGeneratingQR = true;
+
+      // Lógica para generar el código QR
+      const profesorInfo = {
+        id: 1,
+        nombre: 'Nombre del Profesor',
+        horaGeneracion: new Date().toLocaleTimeString(),
+        materias: [this.form.value.materia],
+        // Otros datos relevantes del profesor
+      };
+
+      // Limpiar el código QR anterior si lo hay
+      this.qrCodeData = '';
+
+      // Generar el código QR y actualizar qrCodeData
+      QRCode.toDataURL(JSON.stringify(profesorInfo), (err, url) => {
+        if (err) {
+          console.error('Error al generar el código QR:', err);
+        } else {
+          this.qrCodeData = url;
+        }
+        this.isGeneratingQR = false;
+      });
+    }
   }
 
-  //====== Cerrar sesión ====== //
-  
+  // Función para cerrar la sesión del profesor
+  signOut() {
+    this.qrCodeData = ''; //limpia el codigo QR
+    this.form.reset(); //reinicia la función
+    this.form.controls.materia.setErrors(null); //reinicia la validacion
 
-  signOut(){
-    this.firebaseSvc.signOut();
-  }
-
-  //====== BarCode Scanner ====== //
-  texto:string=''
-  constructor(private barcodescanner:BarcodeScanner) {}
-
-  scan(){
-    this.barcodescanner.scan().then(barcodedata=>{
-      console.log("Scaneando...", barcodedata);
-      this.texto=(JSON.stringify(barcodedata));
-    }).catch(err=>{
-      console.log("ERROR AL ESCANEAR!!!!");
-    })
-
+    this.firebaseSvc.signOut(); //cierra sesion
   }
 
   async submit() {
     if (this.form.valid) {
-
       const loading = await this.utilsSvc.loading();
       await loading.present();
 
-      this.firebaseSvc.sendRecoveryEmail(this.form.value.email).then(res => {
-
+      this.firebaseSvc.sendRecoveryEmail(this.form.value.email).then((res) => {
         this.utilsSvc.presentToast({
           message: 'Correo enviado con éxito',
           duration: 1500,
           color: 'primary',
           position: 'middle',
-          icon: 'mail-outline'
+          icon: 'mail-outline',
         });
 
         this.utilsSvc.routerLink('/auth');
         this.form.reset();
-
-      }).catch(error => {
+      }).catch((error) => {
         console.log(error);
 
         this.utilsSvc.presentToast({
@@ -74,13 +91,11 @@ export class ProfilePage implements OnInit {
           duration: 3000,
           color: 'primary',
           position: 'middle',
-          icon: 'alert-circle-outline'
-        })
-
+          icon: 'alert-circle-outline',
+        });
       }).finally(() => {
         loading.dismiss();
-      })
+      });
     }
   }
-  
 }
